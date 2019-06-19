591dbec929b9f5d3291764ded687cf3af38f0b4d
bestzye <vobecka.k@gmail.com>
2019-06-19 10:42:31 +0200

    prava komentaru

--- CZKEUR_conversion_rate.py
+++ CZKEUR_conversion_rate.py
@@ -1,25 +1,25 @@
-import datetime #knihovna pro datum a cas (time stamp)
+import datetime #datum a cas (time stamp)
 import time #knihovna s funkci sleep
-import pandas as pd #knihovna pro analýzu dat pandas
-from yahoofinancials import YahooFinancials #knihovna pro stahovani informaci z Yahoo!Finance
+import pandas as pd #analyza dat pandas
+from yahoofinancials import YahooFinancials #stahovani informaci z Yahoo!Finance
 
 ticker = "EURCZK=X"
 csv_name = "EURCZK_conversion_rate.csv"
 yahoo_currency_rate = YahooFinancials(ticker)
 
 def time_stamp():
-    '''vytvori time_stamp (UTC) ve formátu DD/MM/YYYY HH:MM:SS'''
+    '''vytvori time_stamp (UTC) ve formatu DD/MM/YYYY HH:MM:SS'''
     return(datetime.datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S"))  
 	
 def get_EURCZK(ticker):
 	'''vytvori prvni radek v csv vcetne nazvu sloupcu, indexem je time_stamp, ulozi nove csv'''
-    data = pd.DataFrame({"current_rate": yahoo_currency_rate.get_current_price(), "current_change": yahoo_currency_rate.get_current_change(), "current_change[%]": (yahoo_currency_rate.get_current_percent_change()*100)}, index = [time_stamp()])
+	data = pd.DataFrame({"current_rate": yahoo_currency_rate.get_current_price(), "current_change": yahoo_currency_rate.get_current_change(), "current_change[%]": (yahoo_currency_rate.get_current_percent_change()*100)}, index = [time_stamp()])
     data.to_csv(csv_name)
 	
 def insert_row():
 	'''prida radek v jiz existujicim csv'''
 	data2 = pd.DataFrame([[yahoo_currency_rate.get_current_price(), yahoo_currency_rate.get_current_change(), (yahoo_currency_rate.get_current_percent_change()*100)]],index = [time_stamp()])
-    with open(csv_name, mode = 'a', newline='') as f:
+	with open(csv_name, mode = 'a', newline='') as f:
         data2.to_csv(f, header = False)
 		
 get_EURCZK(ticker)
