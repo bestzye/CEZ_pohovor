50f96e4cdbd518c100962be47f312ec4421b4ea5
bestzye <vobecka.k@gmail.com>
2019-06-19 11:32:37 +0200

    pripadova studie - EUR/CZK data z Yahoo!Finance, CEZ

--- CZKEUR_conversion_rate.py
+++ CZKEUR_conversion_rate.py
@@ -1,23 +1,23 @@
-import datetime #datum a cas (time stamp)
-import time #knihovna s funkci sleep
-import pandas as pd #analyza dat pandas
-from yahoofinancials import YahooFinancials #stahovani informaci z Yahoo!Finance
+import datetime #time stamp
+import time #function sleep
+import pandas as pd #pandas data analysis, csv 
+from yahoofinancials import YahooFinancials #info from Yahoo!Finance
 
 ticker = "EURCZK=X"
 csv_name = "EURCZK_conversion_rate.csv"
 yahoo_currency_rate = YahooFinancials(ticker)
 
 def time_stamp():
-    '''vytvori time_stamp (UTC) ve formatu DD/MM/YYYY HH:MM:SS'''
+    '''creates a time stamp (UTC time) as DD/MM/YYYY HH:MM:SS format'''
     return(datetime.datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S"))  
 
 def get_EURCZK(ticker):
-    '''vytvori prvni radek v csv vcetne nazvu sloupcu, indexem je time_stamp, ulozi nove csv'''
+    '''creates first row with headers and time_stamp as index into new csv file'''
     data = pd.DataFrame({"current_rate": yahoo_currency_rate.get_current_price(), "current_change": yahoo_currency_rate.get_current_change(), "current_change[%]": (yahoo_currency_rate.get_current_percent_change()*100)}, index = [time_stamp()])
     data.to_csv(csv_name)
     
 def insert_row():
-    '''prida radek v jiz existujicim csv'''
+    '''inserts new row into already existing csv file'''
     data2 = pd.DataFrame([[yahoo_currency_rate.get_current_price(), yahoo_currency_rate.get_current_change(), (yahoo_currency_rate.get_current_percent_change()*100)]],index = [time_stamp()])
     with open(csv_name, mode = 'a', newline='') as f:
         data2.to_csv(f, header = False)
